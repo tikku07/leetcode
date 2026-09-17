ffa8fa78853daf322da02104a8647f8b60e1bdbd
tikku07 <darshiniraju2007@gmail.com>
2026-09-18 00:58:08 +0530

    iterative+recursive approach

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -8,6 +8,24 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+ //iterative approach
+class Solution {
+    public ListNode reverseList(ListNode head) {
+      ListNode prev=null;
+      ListNode temp=head;
+      while(temp!=null){
+        ListNode front=temp.next;
+        temp.next=prev;
+        
+        prev=temp;
+        temp=front;
+      }
+      return prev;
+    }
+}
+
+
  //recursive approach
 class Solution {
     public ListNode reverseList(ListNode head) {
@@ -20,3 +38,5 @@ class Solution {
         return newHead;
     }
 }
+
+
