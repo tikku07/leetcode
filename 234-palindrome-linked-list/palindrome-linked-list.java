47034d761db44801816cfec0d314a44d847470e6
tikku07 <darshiniraju2007@gmail.com>
2026-09-18 17:59:00 +0530

    brute force and optimal  palindrome check for linked list
    Implemented a brute force approach to check if a linked list is a palindrome using a stack.

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -8,6 +8,29 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+// Brute force 
+ 
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        Stack <Integer> st= new Stack <>();
+        ListNode temp=head;
+        while(temp!=null){
+            st.push(temp.val);
+              temp=temp.next;
+        }
+        temp=head;
+        while(temp!=null){
+            if(st.peek() != temp.val) return false;
+            st.pop();
+            temp=temp.next;
+        }
+        return true;
+    }
+}
+
+//optimal
+
 class Solution {
     public boolean isPalindrome(ListNode head) {
         ListNode slow=head;
