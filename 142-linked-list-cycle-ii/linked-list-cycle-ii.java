a12cc9de5abc63021952190c8362b21b2c908740
tikku07 <darshiniraju2007@gmail.com>
2026-09-18 13:30:37 +0530

    brute force+optimal
    Implemented a brute force approach to detect cycle in a linked list using a HashMap.

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -9,6 +9,27 @@
  *     }
  * }
  */
+
+//Brute Force
+
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+         HashMap <ListNode,Integer>ht=new HashMap<>();
+        ListNode temp=head;
+        
+        while(temp!=null){
+            if(ht.containsKey(temp)) return temp;
+            else{
+                ht.put(temp,1);
+            }
+            temp=temp.next;
+        }
+        return null;
+    }
+}
+
+//optimal
+
 public class Solution {
     public ListNode detectCycle(ListNode head) {
 
