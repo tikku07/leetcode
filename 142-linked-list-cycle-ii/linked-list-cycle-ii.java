132da10ec648c80296a949c21834eae9db927296
tikku07 <darshiniraju2007@gmail.com>
2026-09-18 13:28:57 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (69.83%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -9,21 +9,27 @@
  *     }
  * }
  */
- 
- //Brute Force
-
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-         HashMap <ListNode,Integer>ht=new HashMap<>();
-        ListNode temp=head;
-        
-        while(temp!=null){
-            if(ht.containsKey(temp)) return temp;
-            else{
-                ht.put(temp,1);
-            }
-            temp=temp.next;
-        }
-        return null;
+
+         ListNode slow=head;
+        ListNode fast=head;
+while(fast!=null && fast.next!=null){
+    slow=slow.next;
+    fast=fast.next.next;
+    if(slow==fast) {
+        slow=head;
+while(slow!=fast){
+    
+    slow=slow.next;
+    fast=fast.next;
+    
     }
+    return slow;
+
+}
+
+    }
+  return null;  
+}
 }
