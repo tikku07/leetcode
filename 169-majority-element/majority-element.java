ed1c36b4acad851cd90e3c3310f845ae14c860e5
tikku07 <darshiniraju2007@gmail.com>
2026-09-26 22:44:15 +0530

    Time: 19 ms (12.92%) | Memory: 54.8 MB (76.89%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,15 +1,19 @@
 class Solution {
     public int majorityElement(int[] nums) {
-         int max=0;
-        int count=0;
-        int element=0;
-        for(int i=0;i<nums.length;i++){
-            element=nums[i];
-            if(count==0) max=element;
-            if(max==element) count++;
-            else count--;
+        HashMap<Integer,Integer> hp=new HashMap<>();
+        //Better 
+        for(int i =0;i<nums.length;i++){
+            if(hp.containsKey(nums[i])){
+                hp.put(nums[i],hp.get(nums[i])+1);
+            }
+            else{
+                 hp.put(nums[i],1);
+            }
+        }
+        for(Integer num:hp.keySet()){
+            if(hp.get(num)>nums.length/2) return num;
 
         }
-        return max;
+        return 0;
     }
 }
